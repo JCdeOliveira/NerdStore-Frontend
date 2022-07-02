732109e20a87c67c9b6d7597d224014602dd742c
Joao C. de Oliveira <joao.deoliveira@me.com>
2022-07-02 15:36:55 +0200

    NX/Angular Frontend - Admin Panel
    - shell component
    - sidebar component
    - categories-list component
    - category model
    - categories service

--- apps/admin/src/app/app.module.ts
+++ apps/admin/src/app/app.module.ts
@@ -1,14 +1,44 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { RouterModule, Routes } from '@angular/router';
+import { HttpClientModule } from '@angular/common/http';
+
+import { ButtonModule } from 'primeng/button';
+import { CardModule } from 'primeng/card';
+import { ToolbarModule } from 'primeng/toolbar';
+import { TableModule } from 'primeng/table';
 
 import { AppComponent } from './app.component';
-import { RouterModule } from '@angular/router';
+import { ShellComponent } from './shared/shell/shell.component';
 import { DashboardComponent } from './pages/dashboard/dashboard.component';
+import { SidebarComponent } from './shared/sidebar/sidebar.component';
+import { CategoriesListComponent } from './categories/categories-list/categories-list.component';
+import { CategoriesService } from '@silent-jayh/products';
+
+const UX_MODULE = [ButtonModule, CardModule, ToolbarModule, TableModule];
+
+const routes: Routes = [
+    {
+        path: '',
+        component: ShellComponent,
+        children: [
+            {
+                path: 'dashboard',
+                component: DashboardComponent
+            },
+            {
+                path: 'categories',
+                component: CategoriesListComponent
+            }
+        ]
+    }
+];
 
 @NgModule({
-    declarations: [AppComponent, DashboardComponent],
-    imports: [BrowserModule, RouterModule.forRoot([], { initialNavigation: 'enabledBlocking' })],
-    providers: [],
+    declarations: [AppComponent, DashboardComponent, ShellComponent, SidebarComponent, CategoriesListComponent],
+    imports: [BrowserModule, BrowserAnimationsModule, HttpClientModule, RouterModule.forRoot(routes, { initialNavigation: 'enabledBlocking' }), ...UX_MODULE],
+    providers: [CategoriesService],
     bootstrap: [AppComponent]
 })
 export class AppModule {}
